8342ac5869edecb939d47e41d754c023e0a44a12
Aritro <29025984+AritroSaha10@users.noreply.github.com>
2021-05-05 17:09:22 -0400

    finished works area

--- src/pages/works.jsx
+++ src/pages/works.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+
 import { InlineIcon } from '@iconify/react';
 import githubIcon from '@iconify-icons/mdi/github';
 import baselineWeb from '@iconify-icons/ic/baseline-web';
+import bxlPlayStore from '@iconify-icons/bx/bxl-play-store';
+
+import weRecycleBanner from "../images/projectThumbnails/WeRecycle_Banner.png";
 
 const cards = [
     {
         name: "My Puja",
-        description: "A full stack web app with organized directory of events / pujas and livestreams directed toward Hindu temple-goers, specifically patrons of the Toronto Kalibari temple.        ",
+        description: "A full stack web app using React and Firebase with an organized directory of events / pujas and livestreams directed toward Hindu temple-goers, specifically patrons of the Toronto Kalibari temple.",
         projectRedirects: [
             {
                 siteName: "GitHub",
@@ -18,71 +22,93 @@ const cards = [
                 siteName: "Website",
                 url: "https://my-puja-production.web.app",
                 icon: <InlineIcon className="icon" icon={baselineWeb} />,
-                color: "white"
+                color: "#00796b"
             },
         ],
-        image: "",
+        image: null,
+        year: "2020/2021"
+    },
+    {
+        name: "Tenant Complaint System",
+        description: "A website using JQuery and Firebase that allows for the filing and management of complaints from tenants.",
+        projectRedirects: [
+            {
+                siteName: "GitHub",
+                url: "https://github.com/AritroSaha10/Tenant-Complaint-System",
+                icon: <InlineIcon className="icon" icon={githubIcon} />,
+                color: "#1976d2"
+            }
+        ],
+        image: null,
         year: "2020"
     },
     {
-        name: "My Puja",
-        description: "A full stack web app with organized directory of events / pujas and livestreams directed toward Hindu temple-goers, specifically patrons of the Toronto Kalibari temple.        ",
+        name: "WeRecycle",
+        description: "An Android app utilizing AndroidX and Firebase that helps you learn to sort your waste with an index of items and an AI scanner.",
         projectRedirects: [
             {
                 siteName: "GitHub",
-                url: "https://github.com/AritroSaha10/MyPuja-Website",
+                url: "https://github.com/AritroSaha10/WeRecycle",
+                icon: <InlineIcon className="icon" icon={githubIcon} />,
+                color: "#1976d2"
+            },
+            {
+                siteName: "Google Play",
+                url: "https://play.google.com/store/apps/details?id=com.aritrosaha.aritr.werecycleapp",
+                icon: <InlineIcon className="icon" icon={bxlPlayStore} />,
+                color: "#00796b"
+            }
+        ],
+        image: null,
+        year: "2019/2020"
+    },
+    {
+        name: "React To-Do List",
+        description: "A basic to-do list using React that supports adding tasks with a due date and marking them as complete.",
+        projectRedirects: [
+            {
+                siteName: "GitHub",
+                url: "https://github.com/AritroSaha10/React-ToDoList",
                 icon: <InlineIcon className="icon" icon={githubIcon} />,
                 color: "#1976d2"
             },
             {
                 siteName: "Website",
-                url: "https://my-puja-production.web.app",
+                url: "https://to-do-list-development.web.app/",
                 icon: <InlineIcon className="icon" icon={baselineWeb} />,
-                color: "white"
+                color: "#00796b"
             },
         ],
-        image: "",
+        image: null,
         year: "2020"
     },
     {
-        name: "My Puja",
-        description: "A full stack web app with organized directory of events / pujas and livestreams directed toward Hindu temple-goers, specifically patrons of the Toronto Kalibari temple.        ",
+        name: "Smart Garage Door Module",
+        description: "A small module using the Particle Photon that can be added to an old garage door system which will allow for integration with Home Assistant using MQTT.",
         projectRedirects: [
             {
                 siteName: "GitHub",
-                url: "https://github.com/AritroSaha10/MyPuja-Website",
+                url: "https://github.com/AritroSaha10/MQTT-Garage-Door-Module",
                 icon: <InlineIcon className="icon" icon={githubIcon} />,
                 color: "#1976d2"
             },
-            {
-                siteName: "Website",
-                url: "https://my-puja-production.web.app",
-                icon: <InlineIcon className="icon" icon={baselineWeb} />,
-                color: "white"
-            },
         ],
-        image: "",
-        year: "2020"
+        image: null,
+        year: "2020/2021"
     },
     {
-        name: "My Puja",
-        description: "A full stack web app with organized directory of events / pujas and livestreams directed toward Hindu temple-goers, specifically patrons of the Toronto Kalibari temple.        ",
+        name: "MMAL Camera",
+        description: "A C++ library that allows for direct control and display of the Raspberry Pi by using low-level hardware libraries to offload intensive tasks onto the GPU.",
         projectRedirects: [
             {
                 siteName: "GitHub",
-                url: "https://github.com/AritroSaha10/MyPuja-Website",
+                url: "https://github.com/AritroSaha10/MMALCamera",
                 icon: <InlineIcon className="icon" icon={githubIcon} />,
                 color: "#1976d2"
             },
-            {
-                siteName: "Website",
-                url: "https://my-puja-production.web.app",
-                icon: <InlineIcon className="icon" icon={baselineWeb} />,
-                color: "white"
-            },
         ],
-        image: "",
-        year: "2020"
+        image: null,
+        year: "2021"
     },
 ]
 
@@ -97,18 +123,18 @@ function About() {
             <div className="cards">
                 {cards.map(({ name, description, projectRedirects, image, year }) => (
                     <div className="card">
-                        {image && <img src={image} alt="Project preview" />}
-                        <div className="nameAndYear">
+                        {image && <img className="thumbnail" src={image} alt="Project preview" />}
+                        <div className="content">
                             <h3>{name}</h3>
 
                             {year && <p className="year">{year}</p>}
+
+                            <p>{description}</p>
                         </div>
 
-                        <p>{description}</p>
-
                         <div className="redirects">
-                            {projectRedirects.map(({ siteName, url, icon }) => (
-                                <a className="projectLink" href={url}>
+                            {projectRedirects.map(({ siteName, url, icon, color }) => (
+                                <a className="projectLink" href={url} style={{ backgroundColor: color }}>
                                     {icon}
                                     {siteName}
                                 </a>
