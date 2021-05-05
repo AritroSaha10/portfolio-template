94c322aaf29f0d9b2d37b542a8389fedcf4f4c94
Aritro <29025984+AritroSaha10@users.noreply.github.com>
2021-05-05 15:08:04 -0400

    setup some responsiveness with works sect

--- src/pages/works.jsx
+++ src/pages/works.jsx
@@ -1,11 +1,52 @@
 import React from 'react';
+import { InlineIcon } from '@iconify/react';
+import githubIcon from '@iconify-icons/mdi/github';
+import baselineWeb from '@iconify-icons/ic/baseline-web';
 
 const cards = [
     {
-        name: "My Cool Project",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sit amet mi ut felis pellentesque iaculis et ac neque. Nulla posuere diam quis malesuada auctor. Duis pretium tortor ac metus congue, id iaculis est suscipit. Sed ornare dolor leo. "
+        name: "My Puja",
+        description: "A full stack web app with organized directory of events / pujas and livestreams directed toward Hindu temple-goers, specifically patrons of the Toronto Kalibari temple.        ",
+        projectRedirects: [
+            {
+                siteName: "GitHub",
+                url: "https://github.com/AritroSaha10/MyPuja-Website",
+                icon: <InlineIcon className="icon" icon={githubIcon} />,
+                color: "#1976d2"
+            },
+            {
+                siteName: "Website",
+                url: "https://my-puja-production.web.app",
+                icon: <InlineIcon className="icon" icon={baselineWeb} />,
+                color: "white"
+            },
+        ],
+        image: "",
+        year: "2020"
+    },
+    {
+        name: "My Puja",
+        description: "A full stack web app with organized directory of events / pujas and livestreams directed toward Hindu temple-goers, specifically patrons of the Toronto Kalibari temple.        ",
+        projectRedirects: [
+            {
+                siteName: "GitHub",
+                url: "https://github.com/AritroSaha10/MyPuja-Website",
+                icon: <InlineIcon className="icon" icon={githubIcon} />,
+                color: "#1976d2"
+            },
+            {
+                siteName: "Website",
+                url: "https://my-puja-production.web.app",
+                icon: <InlineIcon className="icon" icon={baselineWeb} />,
+                color: "white"
+            },
+        ],
+        image: "",
+        year: "2020"
     },
 ]
+
+
 function About() {
     return (
         <section className="works">
@@ -14,13 +55,26 @@ function About() {
             </div>
 
             <div className="cards">
-                {cards.map(({ name, description }) => (
-                    <a href="#">
-                        <div className="card">
+                {cards.map(({ name, description, projectRedirects, image, year }) => (
+                    <div className="card">
+                        {image && <img src={image} alt="Project preview" />}
+                        <div className="nameAndYear">
                             <h3>{name}</h3>
-                            <p>{description}</p>
+
+                            {year && <p className="year">{year}</p>}
                         </div>
-                    </a>
+
+                        <p>{description}</p>
+
+                        <div className="redirects">
+                            {projectRedirects.map(({ siteName, url, icon }) => (
+                                <a className="projectLink" href={url}>
+                                    {icon}
+                                    {siteName}
+                                </a>
+                            ))}
+                        </div>
+                    </div>
                 ))}
             </div>
         </section>
